083565c8721638e69692c542170a81448dff76e4
Piromsurang Rungserichai <piromsurang.r@ku.th>
2019-08-16 16:03:38 +0700

    add id annotation

--- src/main/java/com/example/transights/demo/model/BtsStation.java
+++ src/main/java/com/example/transights/demo/model/BtsStation.java
@@ -3,10 +3,12 @@ package com.example.transights.demo.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class BtsStation {
 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long btsStationId;
 
